ee9c7b7ec9e3d8df7b5ee34dbf9503ce3c7713a1
Cervator <cervator@gmail.com>
2018-12-24 23:01:24 -0500

    Rearrange initial Job DSL examples for more flexibility

--- images/jenkins-plugins/files/init.groovy.d/03_PrepDSL.groovy
+++ images/jenkins-plugins/files/init.groovy.d/03_PrepDSL.groovy
@@ -4,10 +4,17 @@ import javaposse.jobdsl.dsl.DslScriptLoader
 import javaposse.jobdsl.plugin.JenkinsJobManagement
 import jenkins.model.Jenkins
 
-def jobDslScript = new File("/var/jenkins_home/dslScripts/SeedJobDSL.groovy")
+// Make the Job DSL integration optional. Fairly easy to come up with different ways to trigger it. Env vars are simple.
+if (! System.getenv("DSL_REPO")) {
+    println "No DSL repo defined, skipping DSL prep"
+    return
+}
+
+// Create the seed job itself, so it can run and create the "real" DSL-managed jobs
+def jobDslScript = new File("/var/jenkins_home/init.groovy.d/SeedJobDSL.groovy.DSL")
 def workspace = new File("/tmp/dsl")
 def jobManagement = new JenkinsJobManagement(System.out, [:], workspace)
 new DslScriptLoader(jobManagement).runScript(jobDslScript.text)
 
-// Schedule the job to run so the jobs are created when Jenkins starts.
+// Schedule the job to run so the jobs are created when Jenkins starts - or skip this for more control
 Jenkins.instance.getItemByFullName('seed-job', FreeStyleProject.class).scheduleBuild(new UserIdCause())
